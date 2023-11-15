12c57fa0ee74113d338d93a4f829d4876b90513c
SylvesterOtuGraham <grahamsylvester694@gmail.com>
2023-11-15 13:03:53 -0800

    all done1

--- stringcopy.c
+++ stringcopy.c
@@ -3,7 +3,7 @@
  * _strncopy - copy n char
  * @char_ptr1: string argument
  * @char_ptr2: string argument for char_ptr2
- * @a: integer argument
+ * @n: integer argument
  * Return: char_ptr1
  */
 char *_strncopy(char *char_ptr1, const char *char_ptr2, int n)
